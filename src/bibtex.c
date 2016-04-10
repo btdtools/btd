24ffb59701da5c33a18bac43033b4723a3a02516
Mart Lubbers <mart@martlubbers.net>
2016-04-10 09:39:16 +0200

    cleaned up the parser code

--- src/bibtex.c
+++ src/bibtex.c
@@ -10,104 +10,151 @@
 
 #define BUFSIZE 4048
 
-#define EXPECT(ex, c) \
+#define EXPECT(c, err) {\
 	if(c == EOF) asprintf(errmsg, "Early EOF\n"); \
-	else asprintf(errmsg, "Expected " ex " but got '%c'\n", c);
+	else asprintf(errmsg, "Expected " err " but got '%c'\n", c);\
+	bibtex_free(obj);\
+	return NULL;}
+#define SKIP_WHITE(c, istream)\
+	while(isspace(c))\
+		c = fgetc(istream)
 
-char *safe_strdup(const char *s)
+char *bibtex_field_str(bibtex_field field, char *other)
 {
-	char *r = strdup(s);
-	if(r == NULL){
-		die("strup() failed");
+	switch(field){
+	case BIBTEX_FIELD_ADDRESS: return "address";
+	case BIBTEX_FIELD_ANNOTE: return "annote";
+	case BIBTEX_FIELD_AUTHOR: return "author";
+	case BIBTEX_FIELD_BOOKTITLE: return "booktitle";
+	case BIBTEX_FIELD_CHAPTER: return "chapter";
+	case BIBTEX_FIELD_CROSSREF: return "crossref";
+	case BIBTEX_FIELD_EDITION: return "edition";
+	case BIBTEX_FIELD_EDITOR: return "editor";
+	case BIBTEX_FIELD_HOWPUBLISHED: return "howpublished";
+	case BIBTEX_FIELD_INSTITUTION: return "institution";
+	case BIBTEX_FIELD_JOURNAL: return "journal";
+	case BIBTEX_FIELD_KEY: return "key";
+	case BIBTEX_FIELD_MONTH: return "month";
+	case BIBTEX_FIELD_NOTE: return "note";
+	case BIBTEX_FIELD_NUMBER: return "number";
+	case BIBTEX_FIELD_ORGANIZATION: return "organization";
+	case BIBTEX_FIELD_PAGES: return "pages";
+	case BIBTEX_FIELD_PUBLISHER: return "publisher";
+	case BIBTEX_FIELD_SCHOOL: return "school";
+	case BIBTEX_FIELD_SERIES: return "series";
+	case BIBTEX_FIELD_TITLE: return "title";
+	case BIBTEX_FIELD_TYPE: return "type";
+	case BIBTEX_FIELD_VOLUME: return "volume";
+	case BIBTEX_FIELD_YEAR: return "year";
+	case BIBTEX_FIELD_OTHER: return other;
 	}
-	return r;
 }
 
-//
-//struct bibtex_entry {
-//	bibtex_field field;
-//	char *key;
-//	char *value;
-//	struct bibtex_entry *next;
-//};
-//
-//struct bibtex_object {
-//	bibtex_entrytype type;
-//	char *identifier;
-//	struct bibtex_entry *head;
-//};
-
-void skip_whitespace(FILE *istream)
+char *bibtex_entry_str(bibtex_entrytype type)
 {
-	char c;
-	while(isspace((c = fgetc(istream))));
-	ungetc(c, istream);
+	switch(type){
+	case BIBTEX_ENTRY_ARTICLE: return "article";
+	case BIBTEX_ENTRY_BOOK: return "book";
+	case BIBTEX_ENTRY_BOOKLET: return "booklet";
+	case BIBTEX_ENTRY_CONFERENCE: return "conference";
+	case BIBTEX_ENTRY_INBOOK: return "inbook";
+	case BIBTEX_ENTRY_INCOLLECTION: return "incollection";
+	case BIBTEX_ENTRY_INPROCEEDINGS: return "inproceedings";
+	case BIBTEX_ENTRY_MANUAL: return "manual";
+	case BIBTEX_ENTRY_MASTERSTHESIS: return "mastersthesis";
+	case BIBTEX_ENTRY_MISC: return "misc";
+	case BIBTEX_ENTRY_PHDTHESIS: return "phdthesis";
+	case BIBTEX_ENTRY_PROCEEDINGS: return "proceedings";
+	case BIBTEX_ENTRY_TECHREPORT: return "techreport";
+	case BIBTEX_ENTRY_UNPUBLISHED: return "unpublished";
+	case BIBTEX_ENTRY_UNKNOWN: return NULL;
+	}
 }
+
+bibtex_entrytype bibtex_str_entry(char *str)
+{
+	if(strcmp(str, "article") == 0) return BIBTEX_ENTRY_ARTICLE;
+	if(strcmp(str, "book") == 0) return BIBTEX_ENTRY_BOOK;
+	if(strcmp(str, "booklet") == 0) return BIBTEX_ENTRY_BOOKLET;
+	if(strcmp(str, "conference") == 0) return BIBTEX_ENTRY_CONFERENCE;
+	if(strcmp(str, "inbook") == 0) return BIBTEX_ENTRY_INBOOK;
+	if(strcmp(str, "incollection") == 0) return BIBTEX_ENTRY_INCOLLECTION;
+	if(strcmp(str, "inproceedings") == 0) return BIBTEX_ENTRY_INPROCEEDINGS;
+	if(strcmp(str, "manual") == 0) return BIBTEX_ENTRY_MANUAL;
+	if(strcmp(str, "mastersthesis") == 0) return BIBTEX_ENTRY_MASTERSTHESIS;
+	if(strcmp(str, "misc") == 0) return BIBTEX_ENTRY_MISC;
+	if(strcmp(str, "phdthesis") == 0) return BIBTEX_ENTRY_PHDTHESIS;
+	if(strcmp(str, "proceedings") == 0) return BIBTEX_ENTRY_PROCEEDINGS;
+	if(strcmp(str, "techreport") == 0) return BIBTEX_ENTRY_TECHREPORT;
+	if(strcmp(str, "unpublished") == 0) return BIBTEX_ENTRY_UNPUBLISHED;
+	return BIBTEX_ENTRY_UNKNOWN;
+}
+
+bibtex_field bibtex_str_field(char *str)
+{
+	if(strcmp(str, "address") == 0) return BIBTEX_FIELD_ADDRESS;
+	if(strcmp(str, "annote") == 0) return BIBTEX_FIELD_ANNOTE;
+	if(strcmp(str, "author") == 0) return BIBTEX_FIELD_AUTHOR;
+	if(strcmp(str, "booktitle") == 0) return BIBTEX_FIELD_BOOKTITLE;
+	if(strcmp(str, "chapter") == 0) return BIBTEX_FIELD_CHAPTER;
+	if(strcmp(str, "crossref") == 0) return BIBTEX_FIELD_CROSSREF;
+	if(strcmp(str, "edition") == 0) return BIBTEX_FIELD_EDITION;
+	if(strcmp(str, "editor") == 0) return BIBTEX_FIELD_EDITOR;
+	if(strcmp(str, "howpublished") == 0) return BIBTEX_FIELD_HOWPUBLISHED;
+	if(strcmp(str, "institution") == 0) return BIBTEX_FIELD_INSTITUTION;
+	if(strcmp(str, "journal") == 0) return BIBTEX_FIELD_JOURNAL;
+	if(strcmp(str, "key") == 0) return BIBTEX_FIELD_KEY;
+	if(strcmp(str, "month") == 0) return BIBTEX_FIELD_MONTH;
+	if(strcmp(str, "note") == 0) return BIBTEX_FIELD_NOTE;
+	if(strcmp(str, "number") == 0) return BIBTEX_FIELD_NUMBER;
+	if(strcmp(str, "organization") == 0) return BIBTEX_FIELD_ORGANIZATION;
+	if(strcmp(str, "pages") == 0) return BIBTEX_FIELD_PAGES;
+	if(strcmp(str, "publisher") == 0) return BIBTEX_FIELD_PUBLISHER;
+	if(strcmp(str, "school") == 0) return BIBTEX_FIELD_SCHOOL;
+	if(strcmp(str, "series") == 0) return BIBTEX_FIELD_SERIES;
+	if(strcmp(str, "title") == 0) return BIBTEX_FIELD_TITLE;
+	if(strcmp(str, "type") == 0) return BIBTEX_FIELD_TYPE;
+	if(strcmp(str, "volume") == 0) return BIBTEX_FIELD_VOLUME;
+	if(strcmp(str, "year") == 0) return BIBTEX_FIELD_YEAR;
+	return BIBTEX_FIELD_OTHER;
+}
+
 
 struct bibtex_object *bibtex_parse(FILE *istream, char **errmsg)
 {
 	struct bibtex_object *obj = malloc(sizeof (struct bibtex_object));
+	struct bibtex_entry *current;
 	char buf[BUFSIZE+1];
 	char c;
 	int bufloc = 0;
+	int depth;
+
+	//Init object
+	obj->identifier = NULL;
+	obj->head = NULL;
 
 	//Skip whitespace
-	do {
-		c = fgetc(istream);
-	} while(isspace(c));
+	c = fgetc(istream);
+	SKIP_WHITE(c, istream);
 
 	//Read @
-	if(c != '@'){
-		EXPECT("'@'", c);
-		free(obj);
-		return NULL;
-	}
+	if(c != '@')
+		EXPECT(c, "'@'");
 
 	//Read entrytype
-	while(isalpha(c = fgetc(istream)) && bufloc < BUFSIZE) {
+	while(isalpha(c = fgetc(istream)) && bufloc < BUFSIZE)
 		buf[bufloc++] = c;
-	}
 	buf[bufloc] = '\0';
-	if(strcmp(buf, "article") == 0)
-		obj->type = BIBTEX_ENTRY_ARTICLE;
-	else if(strcmp(buf, "book") == 0)
-		obj->type = BIBTEX_ENTRY_BOOK;
-	else if(strcmp(buf, "booklet") == 0)
-		obj->type = BIBTEX_ENTRY_BOOKLET;
-	else if(strcmp(buf, "conference") == 0)
-		obj->type = BIBTEX_ENTRY_CONFERENCE;
-	else if(strcmp(buf, "inbook") == 0)
-		obj->type = BIBTEX_ENTRY_INBOOK;
-	else if(strcmp(buf, "incollection") == 0)
-		obj->type = BIBTEX_ENTRY_INCOLLECTION;
-	else if(strcmp(buf, "inproceedings") == 0)
-		obj->type = BIBTEX_ENTRY_INPROCEEDINGS;
-	else if(strcmp(buf, "manual") == 0)
-		obj->type = BIBTEX_ENTRY_MANUAL;
-	else if(strcmp(buf, "mastersthesis") == 0)
-		obj->type = BIBTEX_ENTRY_MASTERSTHESIS;
-	else if(strcmp(buf, "misc") == 0)
-		obj->type = BIBTEX_ENTRY_MISC;
-	else if(strcmp(buf, "phdthesis") == 0)
-		obj->type = BIBTEX_ENTRY_PHDTHESIS;
-	else if(strcmp(buf, "proceedings") == 0)
-		obj->type = BIBTEX_ENTRY_PROCEEDINGS;
-	else if(strcmp(buf, "techreport") == 0)
-		obj->type = BIBTEX_ENTRY_TECHREPORT;
-	else if(strcmp(buf, "unpublished") == 0)
-		obj->type = BIBTEX_ENTRY_UNPUBLISHED;
-	else {
+	if((obj->type = bibtex_str_entry(buf)) == BIBTEX_ENTRY_UNKNOWN){
 		asprintf(errmsg, "Invalid bibtex entry type: '%s'\n", buf);
-		free(obj);
+		bibtex_free(obj);
 		return NULL;
 	}
 
-	//Read openbrace and identifier
-	if(c != '{'){
-		EXPECT("'{'", c);
-		free(obj);
-		return NULL;
-	}
+	//Read openbrace
+	if(c != '{')
+		EXPECT(c, "'{'");
+	//Read ident
 	bufloc = 0;
 	while((isalnum(c = fgetc(istream)) || 
 			c == '|' || c == '+' || c == '_' || c == '-') 
@@ -117,20 +164,21 @@ struct bibtex_object *bibtex_parse(FILE *istream, char **errmsg)
 	buf[bufloc] = '\0';
 	if(bufloc == 0){
 		asprintf(errmsg, "Identifier can't be empty\n");
-		free(obj);
+		bibtex_free(obj);
 		return NULL;
 	}
 	obj->identifier = safe_strdup(buf);
+	
+	//Skip whitespace
+	SKIP_WHITE(c, istream);
 
 	//Read key-valuepairs
-	obj->head = NULL;
 	while(c == ','){
-		struct bibtex_entry *current = malloc(sizeof (struct bibtex_entry));
+		current = malloc(sizeof (struct bibtex_entry));
 		current->key = NULL;
 		//skip whitespace
-		do{
-			c = fgetc(istream);
-		} while(isspace(c));
+		c = fgetc(istream);
+		SKIP_WHITE(c, istream);
 
 		//parse ident
 		bufloc = 0;
@@ -139,193 +187,74 @@ struct bibtex_object *bibtex_parse(FILE *istream, char **errmsg)
 			buf[bufloc++] = c;
 		}
 		buf[bufloc] = '\0';
-		if(strcmp(buf, "address") == 0)
-			current->field = BIBTEX_FIELD_ADDRESS;
-		else if(strcmp(buf, "annote") == 0)
-			current->field = BIBTEX_FIELD_ANNOTE;
-		else if(strcmp(buf, "author") == 0)
-			current->field = BIBTEX_FIELD_AUTHOR;
-		else if(strcmp(buf, "booktitle") == 0)
-			current->field = BIBTEX_FIELD_BOOKTITLE;
-		else if(strcmp(buf, "chapter") == 0)
-			current->field = BIBTEX_FIELD_CHAPTER;
-		else if(strcmp(buf, "crossref") == 0)
-			current->field = BIBTEX_FIELD_CROSSREF;
-		else if(strcmp(buf, "edition") == 0)
-			current->field = BIBTEX_FIELD_EDITION;
-		else if(strcmp(buf, "editor") == 0)
-			current->field = BIBTEX_FIELD_EDITOR;
-		else if(strcmp(buf, "howpublished") == 0)
-			current->field = BIBTEX_FIELD_HOWPUBLISHED;
-		else if(strcmp(buf, "institution") == 0)
-			current->field = BIBTEX_FIELD_INSTITUTION;
-		else if(strcmp(buf, "journal") == 0)
-			current->field = BIBTEX_FIELD_JOURNAL;
-		else if(strcmp(buf, "key") == 0)
-			current->field = BIBTEX_FIELD_KEY;
-		else if(strcmp(buf, "month") == 0)
-			current->field = BIBTEX_FIELD_MONTH;
-		else if(strcmp(buf, "note") == 0)
-			current->field = BIBTEX_FIELD_NOTE;
-		else if(strcmp(buf, "number") == 0)
-			current->field = BIBTEX_FIELD_NUMBER;
-		else if(strcmp(buf, "organization") == 0)
-			current->field = BIBTEX_FIELD_ORGANIZATION;
-		else if(strcmp(buf, "pages") == 0)
-			current->field = BIBTEX_FIELD_PAGES;
-		else if(strcmp(buf, "publisher") == 0)
-			current->field = BIBTEX_FIELD_PUBLISHER;
-		else if(strcmp(buf, "school") == 0)
-			current->field = BIBTEX_FIELD_SCHOOL;
-		else if(strcmp(buf, "series") == 0)
-			current->field = BIBTEX_FIELD_SERIES;
-		else if(strcmp(buf, "title") == 0)
-			current->field = BIBTEX_FIELD_TITLE;
-		else if(strcmp(buf, "type") == 0)
-			current->field = BIBTEX_FIELD_TYPE;
-		else if(strcmp(buf, "volume") == 0)
-			current->field = BIBTEX_FIELD_VOLUME;
-		else if(strcmp(buf, "year") == 0)
-			current->field = BIBTEX_FIELD_YEAR;
-		else {
-			current->field = BIBTEX_FIELD_OTHER;
+		if((current->field = bibtex_str_entry(buf)) == BIBTEX_FIELD_OTHER)
 			current->key = safe_strdup(buf);
-		}
+
 		//skip whitespace
-		while(isspace(c)){
-		   c = fgetc(istream);
-		}
+		SKIP_WHITE(c, istream);
 
 		//parse '='
-		if(c != '='){
-			EXPECT("'='", c);
-			if(current->field = BIBTEX_FIELD_OTHER)
-				free(current->key);
-			free(current);
-			free(obj->identifier);
-			free(obj);
-			return NULL;
-		}
+		if(c != '=')
+			EXPECT(c, "'='");
 
 		//skip whitespace
-		do {
-			c = fgetc(istream);
-		} while(isspace(c));
+		c = fgetc(istream);
+		SKIP_WHITE(c, istream);
+
+		//Parse ident
 		bufloc = 0;
-		if(isdigit(c)){
-			buf[bufloc++] = c;
-			while(isdigit(c = fgetc(istream))){
+		buf[bufloc++] = c;
+		if(isdigit(c))
+			while(isdigit(c = fgetc(istream)))
 				buf[bufloc++] = c;
-			}
-			buf[bufloc] = '\0';
-		} else if (c == '"') {
-			buf[bufloc++] = c;
+		else if (c == '"') {
 			while(c = fgetc(istream)){
 				buf[bufloc++] = c;
-				if(c == '\\') {
+				if(c == '\\')
 					buf[bufloc++] = fgetc(istream);
-				} else if(c == '"') {
+				else if(c == '"')
 					break;
-				}
 			}
 			c = fgetc(istream);
-			buf[bufloc] = '\0';
 		} else if (c == '{') {
-			int depth = 1;
-			buf[bufloc++] = c;
+			depth = 1;
 			while(c = fgetc(istream)){
 				buf[bufloc++] = c;
-				if(c == '\\') {
+				if(c == '\\')
 					buf[bufloc++] = fgetc(istream);
-				} else if(c == '{'){
+				else if(c == '{')
 					depth++;
-				} else if (c == '}'){
-					depth--;
-					if(depth == 0){
+				else if (c == '}')
+					if(--depth == 0)
 						break;
-					}
-				}
 			}
 			c = fgetc(istream);
 		} else {
-			EXPECT("digit, '\"' or '{'", c);
-			if(current->field = BIBTEX_FIELD_OTHER)
-				free(current->key);
-			free(current);
-			free(obj->identifier);
-			free(obj);
+			EXPECT(c, "digit, '\"' or '{'");
+			bibtex_free(obj);
 			return NULL;
 		}
+		buf[bufloc] = '\0';
+
 		//skip whitespace
-		while(isspace(c)){
-		   c = fgetc(istream);
-		}
+		SKIP_WHITE(c, istream);
 
 		current->value = safe_strdup(buf);
 		current->next = obj->head;
 		obj->head = current;
 	}
-	if(c != '}'){
-		EXPECT("'}'", c);
-		free(obj->identifier);
-		free(obj);
-		return NULL;
-	}
+	if(c != '}')
+		EXPECT(c, "'}'");
 	return obj;
 }
 
 void bibtex_print(FILE *ostream, struct bibtex_object *obj)
 {
 	struct bibtex_entry *head = obj->head;
-	fprintf(ostream, "@%s{%s,\n", 
-		obj->type == BIBTEX_ENTRY_ARTICLE ? "article" :
-		obj->type == BIBTEX_ENTRY_BOOK ? "book" :
-		obj->type == BIBTEX_ENTRY_BOOKLET ? "booklet" :
-		obj->type == BIBTEX_ENTRY_CONFERENCE ? "conference" :
-		obj->type == BIBTEX_ENTRY_INBOOK ? "inbook" :
-		obj->type == BIBTEX_ENTRY_INCOLLECTION ? "incollection" :
-		obj->type == BIBTEX_ENTRY_INPROCEEDINGS ? "inproceedings" :
-		obj->type == BIBTEX_ENTRY_MANUAL ? "manual" :
-		obj->type == BIBTEX_ENTRY_MASTERSTHESIS ? "mastersthesis" :
-		obj->type == BIBTEX_ENTRY_MISC ? "misc" :
-		obj->type == BIBTEX_ENTRY_PHDTHESIS ? "phdthesis" :
-		obj->type == BIBTEX_ENTRY_PROCEEDINGS ? "proceedings" :
-		obj->type == BIBTEX_ENTRY_TECHREPORT ? "techreport" :
-		obj->type == BIBTEX_ENTRY_UNPUBLISHED ? "unpublished" : "HUH!",
-		obj->identifier);
+	fprintf(ostream, "@%s{%s\n", bibtex_entry_str(obj->type), obj->identifier);
 	while(head != NULL) {
-		if(head->field == BIBTEX_FIELD_OTHER){
-			fprintf(ostream, "\t%s = %s\n",
-				head->key,
-				head->value);
-		} else {
-			fprintf(ostream, "\t%s = %s\n",
-				head->field == BIBTEX_FIELD_ADDRESS ? "address" :
-				head->field == BIBTEX_FIELD_ANNOTE ? "annote" :
-				head->field == BIBTEX_FIELD_AUTHOR ? "author" :
-				head->field == BIBTEX_FIELD_BOOKTITLE ? "booktitle" :
-				head->field == BIBTEX_FIELD_CHAPTER ? "chapter" :
-				head->field == BIBTEX_FIELD_CROSSREF ? "crossref" :
-				head->field == BIBTEX_FIELD_EDITION ? "edition" :
-				head->field == BIBTEX_FIELD_EDITOR ? "editor" :
-				head->field == BIBTEX_FIELD_HOWPUBLISHED ? "howpublished" :
-				head->field == BIBTEX_FIELD_INSTITUTION ? "institution" :
-				head->field == BIBTEX_FIELD_JOURNAL ? "journal" :
-				head->field == BIBTEX_FIELD_KEY ? "key" :
-				head->field == BIBTEX_FIELD_MONTH ? "month" :
-				head->field == BIBTEX_FIELD_NOTE ? "note" :
-				head->field == BIBTEX_FIELD_NUMBER ? "number" :
-				head->field == BIBTEX_FIELD_ORGANIZATION ? "organization" :
-				head->field == BIBTEX_FIELD_PAGES ? "pages" :
-				head->field == BIBTEX_FIELD_PUBLISHER ? "publisher" :
-				head->field == BIBTEX_FIELD_SCHOOL ? "school" :
-				head->field == BIBTEX_FIELD_SERIES ? "series" :
-				head->field == BIBTEX_FIELD_TITLE ? "title" :
-				head->field == BIBTEX_FIELD_TYPE ? "type" :
-				head->field == BIBTEX_FIELD_VOLUME ? "volume" :
-				head->field == BIBTEX_FIELD_YEAR ? "year" : "HUH!",
-				head->value);
-		}
+		fprintf(ostream, "\t,%s = %s\n",
+				bibtex_field_str(head->field, head->key), head->value);
 		head = head->next;
 	}
 	fprintf(ostream, "}");
@@ -333,13 +262,12 @@ void bibtex_print(FILE *ostream, struct bibtex_object *obj)
 
 void bibtex_free(struct bibtex_object *obj)
 {
-	free(obj->identifier);
 	struct bibtex_entry *head = obj->head;
 	struct bibtex_entry *next;
+	free(obj->identifier);
 	while(head != NULL) {
-		if(head->field == BIBTEX_FIELD_OTHER){
+		if(head->field == BIBTEX_FIELD_OTHER)
 			free(head->key);
-		}
 		free(head->value);
 		next = head->next;
 		free(head);
@@ -347,17 +275,3 @@ void bibtex_free(struct bibtex_object *obj)
 	}
 	free(obj);
 }
-
-//Small stub for testing the parser
-//int main(void)
-//{
-//	char *errmsg;
-//	struct bibtex_object *bt = bibtex_parse(stdin, &errmsg);
-//	if(bt == NULL){
-//		printf("err: %s\n", errmsg);
-//	} else {
-//		bibtex_print(stdout, bt);
-//		bibtex_free(bt);
-//	}
-//	return 0;
-//}
